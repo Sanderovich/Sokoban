03064c9d8ace6f16f6b7fa4b3f530fffcdb4dd1e
planktogoly <marijndame1234@live.nl>
2018-09-17 15:31:50 +0200

    Updated code

--- Sokoban/SokobanParser.cs
+++ Sokoban/SokobanParser.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 namespace Sokoban.Field
 {
     class SokobanParser
-    {
+    {   
+        public void Parse(int idMaze)
+        {
+            String[] lines = System.IO.File.ReadAllLines($@"./Mazes/maze_{idMaze}.txt");
+        } 
+        
     }
 }
